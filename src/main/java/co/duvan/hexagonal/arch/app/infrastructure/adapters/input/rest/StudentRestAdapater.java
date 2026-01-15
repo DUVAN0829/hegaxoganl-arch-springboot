7b6a1c18d176f48719d500f43b0e1f3d521efa93
DUVAN0829 <duvthan067@gmail.com>
2026-01-15 12:39:01 -0500

    Implementa funcionalidad de métopdos handler o de controlador en la capa de infrastructure

---
+++ src/main/java/co/duvan/hexagonal/arch/app/infrastructure/adapters/input/rest/StudentRestAdapater.java
@@ -0,0 +1,50 @@
+package co.duvan.hexagonal.arch.app.infrastructure.adapters.input.rest;
+
+import co.duvan.hexagonal.arch.app.application.ports.input.StudentServicePort;
+import co.duvan.hexagonal.arch.app.infrastructure.adapters.input.rest.mapper.StudentRestMapper;
+import co.duvan.hexagonal.arch.app.infrastructure.adapters.input.rest.model.request.StudentCreateRequest;
+import co.duvan.hexagonal.arch.app.infrastructure.adapters.input.rest.model.response.StudentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@RequiredArgsConstructor
+public class StudentRestAdapater {
+
+    private final StudentServicePort servicePort;
+    private final StudentRestMapper mapper;
+
+    //* Handler Methods
+    @GetMapping("/v1/api")
+    public List<StudentResponse> findAll() {
+        return this.mapper.listStudentResponse(this.servicePort.findAll());
+    }
+
+    @GetMapping("/v1/api/{id}")
+    public StudentResponse findById(@PathVariable Long id) {
+        return this.mapper.studentResponse(this.servicePort.findById(id));
+    }
+
+    @PostMapping("/v1/api")
+    public ResponseEntity<StudentResponse> save(@RequestBody StudentCreateRequest studentRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(mapper.studentResponse(servicePort.save(mapper.toStudent(studentRequest))));
+    }
+
+    @PutMapping("/v1/api/{id}")
+    public StudentResponse update(@PathVariable Long id, @RequestBody StudentCreateRequest studentRequest) {
+        return mapper.studentResponse(servicePort.update(id, mapper.toStudent(studentRequest)));
+    }
+
+    @DeleteMapping("/v1/api/{id}")
+    public void deleteById(@PathVariable Long id) {
+        servicePort.deleteByid(id);
+    }
+
+}
